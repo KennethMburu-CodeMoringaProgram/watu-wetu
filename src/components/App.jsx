1a6ead11e64bd0d2d88a903cbea7f87e8e9b0e6c
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-01-18 10:35:34 +0300

    add repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,12 @@ import '../styles/app.css';
 
 import People from './People';
 
+// function showAdditional(additional) {
+//     const alertInformation = Object.entries(additional)
+//       .map(information => `${information[0]}: ${information[1]}`)
+//       .join('\n');
+//     alert(alertInformation)
+//   };
 const App = () => {
 
     return <div className="mt-2 container-fluid">
@@ -11,7 +17,14 @@ const App = () => {
 
         <main id="app-main">
             <div className="row">
+                <div className="banner">
+                    <h1 className="maumau">Watu Wetu MauMau</h1>
                 <People endpoint="kapenguria-six" />
+                </div>
+                <div className='cart'>
+                    <h1 className='Music'>Music</h1>
+                <People endpoint="OldSchool-Musix" />
+                </div>
 
                 <People endpoint="culture" />
             </div>
--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,5 +1,5 @@
 
-const Member = ({ dob, dod, name, image, description }) => {
+const Member = ({ dob, dod, name, Featured_artists,Featured_artist,Genre,Artist ,Nomination ,description, showAdditional ,image, Released, Award }) => {
     return <div className="card">
         <img src={image} alt="" className="card-img-top" />
 
@@ -7,10 +7,18 @@ const Member = ({ dob, dod, name, image, description }) => {
             <h5 className="card-title">{name}</h5>
             <div className="card-text">
                 <p className="lead">{description}</p>
-
+                <p>{Artist}</p>
+                <p>{Released}</p>
+                <p>{Award}</p>
+                <p>{Featured_artist}</p>
+                <p>{Nomination}</p>
+                <p>{Genre}</p>
+                <p>{Featured_artists}</p>
+                 
                 <div className="d-flex justify-content-between">
                     <strong className="text-warning">{dob}</strong>
                     <strong className="text-danger">{dod}</strong>
+                    {/* <button className="btn btn-success" onClick={() => showAdditional(additional)}>More Info</button> */}
                 </div>
             </div>
         </div>
